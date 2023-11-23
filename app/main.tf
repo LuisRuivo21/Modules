b6a8a8915cf51a6b277c8e91d08ed11dc9b17751
LuisRuivo21 <luis.ruivo@apprentice.ua92.co.uk>
2023-11-23 14:03:45 +0000

    updated

--- app/main.tf
+++ app/main.tf
@@ -27,15 +27,15 @@ terraform {
 # REUSABLE MODULES
 #----------------------------------------
 
-# module "alb" {
-#   source         = "github.com/LuisRuivo21/modules//infrastructure/alb?ref=v1.0.0"
-#   ec2_sg_id      = module.sg.ec2_sg_id
-#   vpc_id         = module.network.vpc_id
-#   public_subnets = [for subnet in module.network.public_subnets : subnet.id]
-# }
+module "alb" {
+  source         = "github.com/LuisRuivo21/modules//infrastructure/alb"
+  ec2_sg_id      = module.sg.ec2_sg_id
+  vpc_id         = module.network.vpc_id
+  public_subnets = [for subnet in module.network.public_subnets : subnet.id]
+}
 
 module "asg" {
-  source          = "github.com/LuisRuivo21/modules//infrastructure/asg?ref=v1.0.0"
+  source          = "github.com/LuisRuivo21/modules//infrastructure/asg"
   private_subnets = [for subnet in module.network.private_subnets : subnet.id]
   alb_tg_http_arn = module.alb.alb_tg_http_arn
   ssh_sg_id       = [module.sg.ssh_sg_id]
@@ -44,15 +44,15 @@ module "asg" {
 }
 
 module "ec2" {
-  source         = "github.com/LuisRuivo21/modules//infrastructure//ec2?ref=v1.0.0"
+  source         = "github.com/LuisRuivo21/modules//infrastructure//ec2"
   ssh_alb_sg_ids = module.sg.ssh_alb_sg_ids
 }
 
 module "network" {
-  source = "github.com/LuisRuivo21/modules//infrastructure/network?ref=v1.0.0"
+  source = "github.com/LuisRuivo21/modules//infrastructure/network"
 }
 
 module "sg" {
-  source = "github.com/LuisRuivo21/modules//infrastructure/asg?ref=v1.0.0"
+  source = "github.com/LuisRuivo21/modules//infrastructure/asg"
   vpc_id = module.network.vpc_id
 }
